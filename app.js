b0748da9f76d6ed721623ff5a161b57b7e535c8f
Rahul <rk0484151@gmail.com>
2022-06-14 16:00:52 +0530

    database added

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-//jshint esversion:6
+require('dotenv').config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
 //lodash module
 const _ = require('lodash');
 const ejs = require("ejs");
+const mongoose =require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "This is a simple Blog Website where you can compose a blog and post it.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const resultNotfound = "The content You are looking is not found. Please try to search with another "
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,15 +18,31 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//variable to store post request;
-let posts=[];
- 
+
+//connecting to mongodb
+mongoose.connect(process.env.connectdbs,{ useNewUrlParser:true});
+
+//mongoose schema
+
+const postSchema = new mongoose.Schema({
+  title:String,
+  content:String
+});
+
+//mongoose model
+
+const Post= mongoose.model("Post",postSchema);
+  
  
 app.get("/", (req, res) => {
-  res.render("home",{
-    Home:homeStartingContent,
-    posts:posts});
- 
+
+  Post.find({},(err,posts)=>{
+    res.render("home",{
+      Home:homeStartingContent,
+      posts:posts
+    });
+  })
+
 });
 
 app.get("/contact",(req,res)=>{
@@ -38,45 +55,40 @@ app.get("/about",(req,res)=>{
 
  
 app.get("/compose",(req,res)=>{
-  res.render('compose', {});
+  res.render('compose', {successalert:''});
 });
 
 
 app.post('/compose',(req,res)=>{
-const post={
-    title:req.body.compose_heading,
-    content:req.body.compose_textarea
-  };
-  posts.push(post);
 
-  res.redirect('/');
+const post = new Post({
+       title:req.body.compose_heading,
+       content:req.body.compose_textarea
+})
+post.save(function(err){
+  if (!err) {
+   res.render('compose',{successalert:"Post successfully published!"});
+  }
+  else{
+    res.render('compose',{successalert:"Something went wrong!"});
+  }
+})
 });
 
 
 app.get('/post/:postreq',(req,res)=>{
  const request=_.lowerCase(req.params.postreq);
 
- posts.forEach(element => {
-   const postrequest=_.lowerCase(element.title);
-   if(postrequest==request){
-     res.render('post',{
-       title:element.title,
-       content:element.content
-     });
-   } 
-   
- });
+    Post.findOne({title:request},(err,result)=>{
+      if(result){
+        res.render('post',{title:result.title,content:result.content});
+      }
+      else{
+        res.render('post',{title:'Result Not Found!',content:resultNotfound})
+      }
+    })
+
 });
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
